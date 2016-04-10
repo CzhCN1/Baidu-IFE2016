de0a306fbcff67e2ce2045811a4bca2791479c2d
CzhCN1 <2021525982@qq.com>
2016-04-10 21:12:49 +0800

    mission-35 final

--- mission-35/mission-35.js
+++ mission-35/mission-35.js
@@ -1,4 +1,4 @@
-	var doingFlag = false,
+	var doingFlag = false,	//动作执行标志
 	      pos_x = 1,		//当前的横坐标
 	      pos_y = 1,		//当前的纵坐标
 	      dir = [0,90,180,270,360],	//方向列表
@@ -8,47 +8,47 @@
 	function positionFoward(dirFoward){
 		switch(dirFoward){
 	 	case 0:
-	 		if (pos_y === 1) {
+	 		if (pos_y === 1) {	//边界检测
 	 			break;
 	 		}else{
 	 			pos_y--;
 	 		}
 	 		break;
 	 	case 1:
-	 		if (pos_x === 10) {
+	 		if (pos_x === 10) {	//边界检测
 	 			break;
 	 		}else{
 	 			pos_x++;
 	 		}
 	 		break;
 	 	case 2:
-	 		if (pos_y === 10) {
+	 		if (pos_y === 10) {	//边界检测
 	 			break;
 	 		}else{
 	 			pos_y++;
 	 		}
 	 		break;
 	 	case 3:
-	 		if (pos_x === 1) {
+	 		if (pos_x === 1) {	//边界检测
 	 			break;
 	 		}else{
 	 			pos_x--;
 	 		}
 	 		break;
 	 	}
-	 	console.log(pos_x+":"+pos_y);
 	}
 
-	//转向动画`
+	//转向动画
 	//输入为方向的下标
 	function animateDir(now_dir,target_dir){
 	 	var square = document.getElementById('square'),
-	 	      speed,
-	 	      clockwise,
+	 	      speed,		//转向速度
+	 	      clockwise,		//true顺时针旋转 false逆时针
 	 	      timer,
-	 	      degree ;
+	 	      degree ;		//旋转的角度
 
-	 	square.style.transform = 'rotate(' + dir[now_dir] + 'deg)';
+/*	 	square.style.transform = 'rotate(' + dir[now_dir] + 'deg)';
+*/
 	 	//当前方向与目标方向相同
 	 	if(dir[target_dir] == dir[now_dir]){
 	 		doingFlag = false;
@@ -96,15 +96,16 @@
 	//根据X,Y坐标,相对原位置移动
 	function move(){
 		var square = document.getElementById('square'),
-		      now_top = parseInt(window.getComputedStyle(square, null).top),
-		      now_left = parseInt(window.getComputedStyle(square, null).left),
-		      target_top = 60 + (pos_y - 1) * 61,
-		      target_left = 60 + (pos_x - 1) * 61,
-		      distance,
+		      now_top = parseInt(window.getComputedStyle(square, null).top),	//当前方块顶边距
+		      now_left = parseInt(window.getComputedStyle(square, null).left),	//当前方块左边距
+		      target_top = 60 + (pos_y - 1) * 61,					//移动后方块顶边距
+		      target_left = 60 + (pos_x - 1) * 61,					//移动后方块左边距
+		      distance,			//保存需要改变的值，top或者left
 		      target_distance,
-		      x_flag,
+		      x_flag,			//布尔量　是否是ｘ方向的移动
 		      timer;
 
+		//判断需要改变的是左边距还是顶边距
 		if(target_top == now_top && target_left == now_left){
 			doingFlag = false;
 			return;
@@ -168,16 +169,17 @@
 	 	}
 	 }
 
-
-
 	(function(){
 		var square = document.getElementById('square'),
 		      do_btn = document.getElementById('do'),
 		      refresh_btn = document.getElementById('refresh'),
 		      text_input = document.getElementById('textarea'),
-		      row_num = 1;
+		      row_num = 1;	//textarea的内容行数
 
-		//textarea键盘按键被按下
+		//初始化
+		refresh();
+
+		//textarea键盘按键被按下事件
 		text_input.addEventListener('keydown',function(){
 			setTimeout(function(){
 				var content = text_input.value,
@@ -201,67 +203,7 @@
 					return;
 				}
 			},0);
-
 		},false);
-
-		//行标更新函数
-		function rowNum(num_before,num_now){
-			var leftCol = document.getElementById("leftCol"),
-			       topValue;
-			while(num_before !== num_now){
-				var pNode;
-				if(num_now < num_before){
-					pNode = leftCol.lastChild;
-					leftCol.removeChild(pNode);
-					num_before--;
-				}else{
-					pNode = document.createElement("p");
-					pNode.innerHTML = num_before + 1;
-					leftCol.appendChild(pNode);
-					num_before++;
-				}
-			}
-
-			if(num_now > 11){
-					topValue =0 - (num_now - 11) * 22 + 8;
-					console.log(topValue);
-					leftCol.style.top = topValue + 'px';
-			}else{
-				leftCol.style.top = 0+'px';
-			}
-		}
-
-		function parseOrder(){
-			var content = text_input.value,	//textarea内容
-			      leftCol = document.getElementById("leftCol"),
-			      orderLists = [],		//逐行解析的内容
-			      numLists = [],		//每条指令 移动格数
-			      rowOrderLists,		//解析单行指令
-			      orders = ["GO","TUN","TRA","MOV","TOP","RIG","BAC","BOT","LEF"]; //正确的指令条目
-			orderLists = content.split("\n");
-			if(orderLists[orderLists.length - 1] === ""){
-				orderLists.pop();
-			}
-			for(var i = 0,len = orderLists.length;i<len;i++){
-				if(orderLists[i].match(/\d/)){
-					numLists[i] = parseInt(orderLists[i].match(/\d/));
-				}else{
-					numLists[i] = 1;
-				}
-				rowOrderLists = [];
-				rowOrderLists = orderLists[i].match(/([A-Za-z]+)/g);
-				console.log(rowOrderLists);
-				if(rowOrderLists.length>2 || (rowOrderLists[0] == "GO" && rowOrderLists[1])  ){
-					leftCol.getElementsByTagName('p')[i].style.background = '#f00';
-				}
-				for(var j = 0;j<rowOrderLists.length;j++){
-					if(orders.indexOf(rowOrderLists[j]) == -1){
-						leftCol.getElementsByTagName('p')[i].style.background = '#f00';
-					}
-				}
-			}
-		}
-		parseOrder();
 
 		//执行按钮点击事件
 		do_btn.addEventListener('click',function(){
@@ -270,69 +212,203 @@
 			}
 
 			doingFlag = true;
-			var square = document.getElementById('square');
-
-			//根据输入执行相应函数
-			switch(1){	//************************************************
-			case "GO":
-		 		positionFoward(dir_now);
-		 		move();
-				break;
-			case "TUN LEF":
-				turnLeft();
-				break;
-			case "TUN RIG":
-				turnRight();
-				break;
-			case "TUN BAC":
-				turnBack();
-				break;
-
-			// TRA
-			case "TRA TOP":
-				positionFoward(0);
-				move();
-				break;
-			case "TRA RIG":
-				positionFoward(1);
-				move();
-				break;
-			case "TRA BOT":
-				positionFoward(2);
-				move();
-				break;
-			case "TRA LEF":
-				positionFoward(3);
-				move();
-				break;
-
-			// MOV
-			case "MOV TOP":
-				animateDir(dir_now,0);
-				positionFoward(0);
-				move();
-				break;
-			case "MOV RIG":
-				animateDir(dir_now,1);
-				positionFoward(1);
-				move();
-				break;
-			case "MOV BOT":
-				animateDir(dir_now,2);
-				positionFoward(2);
-				move();
-				break;
-			case "MOV LEF":
-				animateDir(dir_now,3);
-				positionFoward(3);
-				move();
-				break;
+			var square = document.getElementById('square'),
+			      finalOrder = [],
+			      timer_order,
+			      i = 0;
+			finalOrder = parseOrder();
+			console.log(finalOrder);
+			function delayDo(){
+				doOrder(finalOrder[i]);
+				if(finalOrder[i+1]){
+					i++;
+					setTimeout(delayDo,500);
+				}else{
+					return;
+				}
 			}
+			delayDo();
 		},false);
 
 		//Refresh按钮点击事件
-		refresh_btn.addEventListener('click',function(){
+		refresh_btn.addEventListener('click',refresh,false);
 
-		},false);
+		function refresh(){
+			var content = text_input.value,
+			      nNum,
+			      square = document.getElementById('square');
+			if(doingFlag){
+				return;
+			}
+			pos_x = 1;		//当前的横坐标
+			pos_y = 1;		//当前的纵坐标
+			dir_now = 0;		//当前的方向下标
+			square.style.left = 60 + 'px';
+			square.style.top = 60 + 'px';
+			square.style.transform = 'rotate(' + 0 + 'deg)';
+
+			//若未匹配到换行则正则匹配返回NULL，没有length属性
+			//故先判断是否有换行
+			if( content.match(/\n/g) ){
+				nNum = content.match(/\n/g).length;
+			}else{
+				nNum = 0;
+			}
+
+			//行数为换行数+1
+			//判断换行符的数目是否变化
+			if(row_num !==  nNum + 1){
+				//更新行标
+				rowNum(row_num, nNum + 1 );
+				row_num = nNum + 1;
+
+			}else{
+				return;
+			}
+		}
 
 	}());
+
+	//行标更新函数
+	function rowNum(num_before,num_now){
+		var leftCol = document.getElementById("leftCol"),
+		       topValue;
+		while(num_before !== num_now){
+			var pNode;
+			if(num_now < num_before){
+				pNode = leftCol.lastChild;
+				leftCol.removeChild(pNode);
+				num_before--;
+			}else{
+				pNode = document.createElement("p");
+				pNode.innerHTML = num_before + 1;
+				leftCol.appendChild(pNode);
+				num_before++;
+			}
+		}
+
+		if(num_now > 11){
+				topValue =0 - (num_now - 11) * 22 + 8;
+				console.log(topValue);
+				leftCol.style.top = topValue + 'px';
+		}else{
+			leftCol.style.top = 0+'px';
+		}
+	}
+
+	//命令解析检错
+	function parseOrder(){
+		var text_input = document.getElementById('textarea'),
+		      content = text_input.value,	//textarea内容
+		      leftCol = document.getElementById("leftCol"),
+		      orderLists = [],		//逐行解析的内容
+		      numRepeat ,		//每条指令 移动格数
+		      rowOrderLists,		//解析单行指令
+		      finalOrder = [], 		//最终需执行的命令列表
+		      errFlag = false,		//命令非法标志
+		      orders = ["GO","TUN","TRA","MOV","TOP","RIG","BAC","BOT","LEF"]; //正确的指令条目
+
+		orderLists = content.split("\n");		//用换行符分割内容
+
+		for(i = 0,len = orderLists.length;i<len;i++){
+			if(orderLists[i].match(/\d/)){	//用正则匹配命令的执行次数
+				numRepeat = parseInt(orderLists[i].match(/\d/));
+				if(numRepeat>9){	//如果执行次数超过９次按９次执行
+					numRepeat = 9;
+				}
+			}else{
+				numRepeat = 1;		//如果没有输入执行次数　默认为１
+			}
+			rowOrderLists = [];
+			rowOrderLists = orderLists[i].match(/([A-Za-z]+)/g);	//正则匹配分解一行命令保存至数组
+
+			//命令格式判断　命令为空，ＧＯ命令同行还有其他指令，其他运动命令后没有跟方向指令均判断为错误
+			if( !rowOrderLists || rowOrderLists.length>2 || (rowOrderLists[0] == "GO" && rowOrderLists[1]) || (rowOrderLists[0] !== "GO" && !rowOrderLists[1]) ){
+				leftCol.getElementsByTagName('p')[i].style.background = '#f00';
+			}else{
+				//判断行内的指令是否包换在合法指令数组内
+				for(var j = 0;j<rowOrderLists.length;j++){
+					if(orders.indexOf(rowOrderLists[j]) == -1){
+						leftCol.getElementsByTagName('p')[i].style.background = '#f00';
+						errFlag = true;
+						break;
+					}
+				}
+				//将合法指令压入最终指令数组中
+				if(!errFlag){
+					leftCol.getElementsByTagName('p')[i].style.background = '';
+					while(numRepeat){	//若指令重复多次，则把指令多次压入
+						numRepeat--;
+						if(rowOrderLists[1]){
+							finalOrder.push(rowOrderLists[0]+" "+rowOrderLists[1]);
+						}else{
+							finalOrder.push(rowOrderLists[0]);
+						}
+					}
+
+				}
+				errFlag = false;
+			}
+		}
+		return finalOrder;
+	}
+
+	//根据输入命令执行
+	function doOrder(order){
+		//根据输入执行相应函数
+		switch(order){	//************************************************
+		case "GO":
+	 		positionFoward(dir_now);
+	 		move();
+			break;
+		case "TUN LEF":
+			turnLeft();
+			break;
+		case "TUN RIG":
+			turnRight();
+			break;
+		case "TUN BAC":
+			turnBack();
+			break;
+
+		// TRA
+		case "TRA TOP":
+			positionFoward(0);
+			move();
+			break;
+		case "TRA RIG":
+			positionFoward(1);
+			move();
+			break;
+		case "TRA BOT":
+			positionFoward(2);
+			move();
+			break;
+		case "TRA LEF":
+			positionFoward(3);
+			move();
+			break;
+
+		// MOV
+		case "MOV TOP":
+			animateDir(dir_now,0);
+			positionFoward(0);
+			move();
+			break;
+		case "MOV RIG":
+			animateDir(dir_now,1);
+			positionFoward(1);
+			move();
+			break;
+		case "MOV BOT":
+			animateDir(dir_now,2);
+			positionFoward(2);
+			move();
+			break;
+		case "MOV LEF":
+			animateDir(dir_now,3);
+			positionFoward(3);
+			move();
+			break;
+		}
+	}
